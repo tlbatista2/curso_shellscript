9fe9123ebbf6de17538d16dc34178c2db764f1ef
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2025-03-08 12:13:53 -0300

    modificacao

---
+++ variaveis/variaveis.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+nome="Tiago"
+idade=37
+
+
+echo "Olá, $nome!"
+echo "Você tem $idade anos."
+
+echo "Seu diretório home é: $HOME"
+echo "Seu usuário é: $USER"
+echo "Seu path é: $USER"
+
+# interpolação com chaves
+echo "Olá, ${nome}"
+echo "Olá,  ${nome}123"
